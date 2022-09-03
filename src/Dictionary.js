a5b59b108641080305258916c2bebf792dca870c
JennyGlover <81665624+JennyGlover@users.noreply.github.com>
2022-09-03 02:01:52 -0400

    finished search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,29 +1,34 @@
 import React, {useState} from "react";
-import "./Dictionary.css";
 import axios from "axios";
+import "./Dictionary.css";
+import Results from "./Results";
+
 
 export default function Dictionary(){
-    let [keyword, setkeyword] = useState('');
+    let [keyword, setKeyword] = useState('');
+    let [results, setResults]= useState(null)
     function handleResponse(response){
-        
+        console.log(response.data[0]);
+        setResults(response.data[0])
     }
     function search(event){
         event.preventDefault();
-        alert(`Searching for ${keyword}`);
 
     }
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse)
+    //https://api.dictionaryapi.dev
+    let apiUrl = `http://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
 
 
     function handleKeywordChange(event){
-        setkeyword(event.target.value);
+        setKeyword(event.target.value);
     }
     return(
         <div className="Dictionary">
           <form className="search-form" onSubmit={search}>
            <input className="search-input" type="search" autoFocus={true} onChange={handleKeywordChange}/>
          </form>
+         <Results results={results} />
         </div>
     ) 
 }
